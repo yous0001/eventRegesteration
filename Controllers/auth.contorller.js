50c1fdfa68acbf2dc4a5d3a3f5f35e9aae498bed
Yousef emad mikhail <mad33833@gmail.com>
2025-03-15 16:18:02 +0200

    add: login  api

--- Controllers/auth.contorller.js
+++ Controllers/auth.contorller.js
@@ -38,15 +38,37 @@ export const verifyEmail=async(req, res, next) => {
     try {
         const { token } = req.query;
         const decodedToken = jwt.verify(token, process.env.JWT_SECRET_VERFICATION);
-        const user = await UserModel.findOne({ email: decodedToken.email });
+        const user = await UserModel.findOne({ email: decodedToken.email ,isEmailVerified:false});
 
         if (!user) {
             return res.status(404).json({ message: 'User not found' });
         }
-        
+
         user.isEmailVerified = true;
         await user.save();
         res.status(200).json({ message: 'Email verified successfully' });    
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'internal server error' });
+    }
+}
+
+export const login=async(req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        const user = await UserModel.findOne({ email });
+        if (!user) {
+            return res.status(401).json({ message: 'Invalid credentials' });
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if (!isPasswordCorrect) {
+            return res.status(401).json({ message: 'Invalid credentials' });
+        }
+        const token = jwt.sign({id:user._id }, process.env.JWT_SECRET_AUTH, { expiresIn: '1d' });
+
+        res.status(200).json({ message: 'Login successful',token });
+        
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'internal server error' });
--- Routes/auth.routes.js
+++ Routes/auth.routes.js
@@ -5,6 +5,6 @@ const router=Router()
 
 router.post('/register',authController.register) 
 router.get('/verify-email',authController.verifyEmail) 
-
+router.post('/login',authController.login)
 
 export default router
